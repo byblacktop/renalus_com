9c19941e4fe0aa7b46d6700412c84d3414e25483
Ralph Haynes <ralph@builtbyblacktop.com>
2025-06-27 23:52:28 -0500

    (Cleanup): Remove unsued logic

--- app/_components/TagManager.jsx
+++ app/_components/TagManager.jsx
@@ -1,5 +1,5 @@
 // import { headers } from 'next/headers'
-import { GoogleTagManager } from '@next/third-parties/google'
+// import { GoogleTagManager } from '@next/third-parties/google'
 import { PrismicPreview } from '@prismicio/next'
 import { Analytics } from '@vercel/analytics/react'
 import { VercelToolbar } from '@vercel/toolbar/next'
@@ -8,21 +8,10 @@ import { repositoryName } from '@/prismicio'
 
 // Ref: https://nextjs.org/docs/app/building-your-application/optimizing/third-party-libraries#google-tag-manager
 // TODO: Setup tag manager
-const GOOGLE_TAG_MANAGER_ID = process.env.NEXT_PUBLIC_GTM_CONTAINER_ID
+// const GOOGLE_TAG_MANAGER_ID = process.env.NEXT_PUBLIC_GTM_CONTAINER_ID
 
 const TagManager = () => {
-	// TODO: Remove when no longer in development
-	// if (process.env.NODE_ENV === 'development') return
-
-	// TODO: Add when no longer in development
 	const isStaging = process.env.NODE_ENV === 'staging'
-
-	// const referer = headers().get('referer')
-	// const showToolbar = !referer?.includes(
-	// 	process.env.NEXT_PUBLIC_SITE_URL,
-	// )
-	// TODO: Figure out why above doesn't work
-	// const showToolbar = true
 
 	return (
 		<>
